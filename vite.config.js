ac05a8d801ebb8657a5bf77d1730c76c975b2d1f
implemently <146377581+implemently@users.noreply.github.com>
2025-04-06 18:43:28 -0400

    Fix missing index.html and Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This defines where the built files will be placed
+  },
 });
